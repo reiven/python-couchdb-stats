cfae4a501aeb5633f5b9f9ddf25def14a6e626be
reiven <reiven@gmail.com>
2013-04-12 18:11:37 -0300

    added Optparse for cmdline

--- couchdb_stats.py
+++ couchdb_stats.py
@@ -1,10 +1,28 @@
 #-*- coding: utf-8 -*-
 from requests.auth import HTTPBasicAuth
 import requests
-import json
+from optparse import OptionParser
 
-# testing: not accepting parameters, only check on localhost
-r = requests.get('http://localhost:5984/_stats')
+parser = OptionParser(usage="usage: %prog [-h] [-P PORT] HOSTNAME")
+parser.add_option("-H", "--hostname",
+                    dest="hostname",
+                    action="store",
+                    default="127.0.0.1",
+                    help="default to localhost")
+parser.add_option("-P", "--port",
+                    dest="port",
+                    action="store",
+                    default="5984",
+                    help="default port [5984]")
+(options, args) = parser.parse_args()
+
+# create the url
+r = requests.get(str.join('', ('http://', options.hostname,
+                ':', options.port, '/_stats')))
+
+# check if the response was valid
+if r.status_code != 200:
+    raise ("connection failed!")
 
 
 def iterator(stats):
